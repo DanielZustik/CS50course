25b0601a239dc606be600f4131dec73ac6c986e3
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-21 11:10:50 +0000

    Tue, Feb 21, 2023, 12:10 PM +01:00

---
+++ population/population.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int n;
+    while (n < 9)
+{
+    n = get_int ("Jaká je počáteční populace?");
+}
+
+
+    // TODO: Prompt for end size
+
+    int stop = get_int ("Jaká je koncová populace?");
+
+    // TODO: Calculate number of years until we reach threshold
+
+    int y = 0;
+    while (n < stop)
+{
+    n = n + n/3 - n/4;
+    y++;
+
+    // TODO: Print number of years
+ }
+
+ printf("Konečná populace bude dosažena za %i let\n", y);
+}
