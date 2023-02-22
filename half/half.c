5dae11e6a6daf4c993132b708e2bfe6dd4cbc6b9
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-22 09:50:13 +0000

    Wed, Feb 22, 2023, 10:50 AM +01:00

--- half/half.c
+++ half/half.c
@@ -4,19 +4,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-float half(float bill, float tax, int tip);
+float half(float bill, float tax, float tip);
 
 int main(void)
 {
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sale Tax Percent: ");
-    int tip_percent = get_int("Tip percent: ");
+    float tip_percent = get_float("Tip percent: ");
 
     printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
 }
 
 // TODO: Complete the function
-float half(float bill, float tax, int tip)
+float half(float bill, float tax, float tip)
 {
     tax = tax/100;
     tip = tip/100;
