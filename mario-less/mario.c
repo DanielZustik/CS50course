4a773c8056e094c6daf70a22ea55bc3c45234d30
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-22 10:16:45 +0000

    Wed, Feb 22, 2023, 11:16 AM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,5 +3,5 @@
 
 int main(void)
 {
-    //uživatel určí jak vysoká má být pyramida v rozmezí 1 až 8
+    //uživatel určí jak vysoká má být pyramida v rozmezí 1 až 8 (bez sdělování, ptání se dokola dokud nezadá)
 }
