88cbd4066cf7b77cb2350faf48fa09a26acfced8
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-23 12:02:05 +0000

    Thu, Feb 23, 2023, 1:02 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,14 +6,8 @@ int main(void)
     //long number = get_long("Number:");
     //printf("%li\n", number);
 
-    long numero = get_long("Number: ");
-    //float test = 1234 /10000; //% 10;
-    long otest = numero - 1000000000000;//12 nul
-    printf("%li\n",otest);
-    if (otest > 1)
-    {
-        printf("jde o 17-ti místné číslo\n");
-    }
-    //printf("%f\n", test);
+
+    long test = 1234 % 10;
+    printf("%li\n", test);
     //dělení modulem 10, 100 atd. dokud nedostaneme zbytek 0
 }
