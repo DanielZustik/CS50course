be6bf0543beb503cbc2c72d3b0616ed654a48c70
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-23 12:47:06 +0000

    Thu, Feb 23, 2023, 1:47 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,19 +3,18 @@
 
 int main(void)
 {
-
-    long test = get_long ("Number:");
-    test = test /10 %10;
-    printf("%li\n", test);
-
     //uložení čísla
     //dělení deseti a modulo 10 a uložení do proměnné
     //"testování, zda zbývající číslo je větší než 10, je li menší, tedy jednomístné, tak konec (už mám uloženy všechny číslice)
+    long number0 = get_long("Number:");
+    long number = number0;
 
+    int mistne = 0;
     do
     {
-        
-
+        number = number/10;
+        mistne = mistne + 1;
     }
     while (number > 10);
+    printf("počet cifer: %i\n", mistne);
 }
