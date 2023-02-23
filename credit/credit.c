e448bce6f389fbc2c751413fbddd4f223a4cd73c
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-23 12:27:14 +0000

    Thu, Feb 23, 2023, 1:27 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,22 +3,9 @@
 
 int main(void)
 {
-    //long number = get_long("Number:");
-    //printf("%li\n", number);
 
-    long test = get_long("Number:");
+    long test = get_long ("Number:");
+    test = test /10 %10;
+    printf("%li\n", test);
 
-    velikost = number - 10000000000000;
-
-    if (velikost > 0)
-    {
-        printf("číslo je );
-    }
-
-    long test = test / 1000000000000; //13-ti
-    long test = test / 10000000000000; //14-ti
-    long test = test / 100000000000000; //15-ti
-    long test = test / 1000000000000000; //16-ti
-    printf("%li\n", test1);
-    //dělení modulem 10, 100 atd. dokud nedostaneme zbytek 0
 }
