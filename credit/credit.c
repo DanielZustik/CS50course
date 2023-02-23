2fe00286ba0a3acf2889f94167e7114305a4065e
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-23 12:34:29 +0000

    Thu, Feb 23, 2023, 1:34 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,4 +8,14 @@ int main(void)
     test = test /10 %10;
     printf("%li\n", test);
 
+    //uložení čísla
+    //dělení deseti a modulo 10 a uložení do proměnné
+    //"testování, zda zbývající číslo je větší než 10, je li menší, tedy jednomístné, tak konec (už mám uloženy všechny číslice)
+
+    do
+    {
+        
+
+    }
+    while (number > 10);
 }
