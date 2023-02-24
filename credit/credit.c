2e28f46ef4aaff89ec3e01853f2f0b69031763ee
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-24 10:55:40 +0000

    Fri, Feb 24, 2023, 11:55 AM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -21,9 +21,6 @@ int main(void)
     printf("počet cifer: %i\n", cifer);
 
 
-
-
-
     if (cifer == 15)            // určí o jaký typ akrty jde
     {
         printf("AMEX\n");
@@ -51,13 +48,16 @@ int main(void)
     }
 
 
-    int zbytek = number0 % 10;          //vytiskne jednotlive cislice
-    printf("zbytek:%i\n", zbytek);
-    int number2 = number0;
-    for (int i = 1; i < cifer; i++)
-    {
-        number2 = number2 / 10;
-        int zbytek2 = number2 % 10;
-        printf("dalsi zbytky: %i\n", zbytek2);
-    }
+
+
+
+    //int zbytek = number0 % 10;          //vytiskne jednotlive cislice
+    //printf("zbytek:%i\n", zbytek);
+    //int number2 = number0;
+    //for (int i = 1; i < cifer; i++)
+    //{
+    //    number2 = number2 / 10;
+    //    int zbytek2 = number2 % 10;
+    //    printf("dalsi zbytky: %i\n", zbytek2);
+    //}
 }
