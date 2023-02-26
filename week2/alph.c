8fb58cbcd07c11e771ef18aacbe40d03b7395862
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-26 13:52:10 +0000

    Sun, Feb 26, 2023, 2:52 PM +01:00

--- week2/alph.c
+++ week2/alph.c
@@ -6,8 +6,28 @@
 
 int main(void)
 {
-    string strg = get_string("String: ");
+    string word = get_string("Word: ");
     // získat velikost pole strlen(string), string.h
     // entrnout každý prvek pole, který je lowercase a srovnat ho s přecházejícím, pokud všechyn splnují podmínku tisknout yes.. a nějak ignorovat uppercase písmena.. tedy checknout zda jde o upper nb lower nejprve,
+
+    string boolean;
+
+        for (i = 1; i < strlen(word); i++)
+        {
+            do
+            {
+                if (word[i] > word[i - 1])
+                {
+                    boolean = "true";
+                }
+                else
+                {
+                    boolean = "false";
+                    i = strlen(word);
+                }
+            while (boolean = "true");
+             }
+    printf("No\n");
+    }
 
 }
