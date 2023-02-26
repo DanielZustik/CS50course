63a8935e0e70a60013a015d1570b9e638a175ee1
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-26 12:34:15 +0000

    Sun, Feb 26, 2023, 1:34 PM +01:00

---
+++ week2/alph.c
@@ -0,0 +1,13 @@
+//zeptání se na string
+//ověření, zda jsou lowercase písmena stringu po sobě jdoucí v abecedném pořadí
+//pokud ano, tisknout ano, pokud ne, tisknout ne
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string strg = get_string("String: ");
+    // získat velikost pole
+    // entrnout každý prvek pole, který je lowercase a srovnat ho s přecházejícím
+
+}
