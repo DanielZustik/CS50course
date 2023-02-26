43e2c6d6969e74c3e95b0f245e9736bb755d2e8f
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-26 11:59:23 +0000

    Sun, Feb 26, 2023, 12:59 PM +01:00

--- week2/arrays.c
+++ week2/arrays.c
@@ -3,16 +3,16 @@
 
 int main(void)
 {
-    int n = get_int("Velikost pole:")
+    int n = get_int("Velikost pole:");
     int pole[n];
     //pole[0] = 1;
     //pole[1] = 2;
     //pole[2] = 4;
     pole[0] = 1;
-    printf("Pole %i = ", pole[0])
-    for(int i = 1, i <=n, i++)
+    printf("Pole 0 = %i\n", pole[0]);
+    for(int i = 1; i < n; i++)
     {
         pole[i] = pole[i-1] * 2;
-        printf("Pole %i = ", i)
+        printf("Pole %i = %i\n", i, pole[i]);
     }
 }
