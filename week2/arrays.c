1edeb1796ff0bd8de1338a720aa9386cd86bca08
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-26 11:50:20 +0000

    Sun, Feb 26, 2023, 12:50 PM +01:00

--- week2/arrays.c
+++ week2/arrays.c
@@ -4,5 +4,12 @@
 int main(void)
 {
     int n = get_int("Velikost pole:")
-    int pole[];
+    int pole[n];
+    //pole[0] = 1;
+    //pole[1] = 2;
+    //pole[2] = 4;
+    for(int i = 1, i <=n, i++)
+    {
+        pole[i] = i 
+    }
 }
