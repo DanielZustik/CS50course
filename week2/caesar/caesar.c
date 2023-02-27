0e2fa7b51e83e6cb4fb31877238820e6996e0308
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-02-27 16:59:18 +0000

    Mon, Feb 27, 2023, 5:59 PM +01:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -21,6 +21,7 @@ int main(int argc, string argv[])
         return 1;
     }
     printf("%c\n", rotate('c', 1));
+    
 }
 
 bool only_digits(string s) // true pokud obsahuje zadané pouze číslice od 0 do 9, false jinak
