a84c0d41cd7c2ad232688d35ad552fa26ed0fded
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-01 10:40:32 +0000

    Wed, Mar 1, 2023, 11:40 AM +01:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(int argc, string argv[])
@@ -9,13 +10,14 @@ int main(int argc, string argv[])
     {
         printf("Usage: ./substitution key\n");
     }
-    if (strlen(argv[1]) != 26)
+    if (strlen(argv[1]) != 6)
     {
-        printf("Key must contain 26 characters.\n");
+        printf("Key must contain 6 characters.\n");
     }
-    for (i = 0; i < strlen(argv[1]); i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        argv[1][i] = tolower(argv[i]);
+        argv[1][i] = tolower(argv[1][i]);
         printf("%c", argv[1][i]);
     }
+    printf("\n");
 }
