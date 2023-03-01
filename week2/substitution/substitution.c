e5db04c0b29364c9c33e68dfe6d5dde6c38baf53
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-01 10:29:53 +0000

    Wed, Mar 1, 2023, 11:29 AM +01:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, string argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+    }
+    if (strlen(argv[]) != 26)
+    {
+        printf("Key must contain 26 characters.");
+    }
 }
