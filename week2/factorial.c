e00b5304f223d36be297780c2036727cf12dbb6f
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-02 16:49:39 +0000

    Thu, Mar 2, 2023, 5:49 PM +01:00

--- week2/factorial.c
+++ week2/factorial.c
@@ -1,10 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int catorial(int parametr);
 
 int main (void)
 {
-
+    int n = get_int("Type a number: ");
+    printf("%i\n", fatorial(n));
 }
 
 int catorial(int parametr)
+{
+
+}
