78c85595c7481822cbd0d238fa3877d95e32210a
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-05 15:46:55 +0000

    Sun, Mar 5, 2023, 4:46 PM +01:00

--- week3/plurality/text.c
+++ week3/plurality/text.c
@@ -12,25 +12,27 @@ typedef struct
 candidate;
 
 // Array of candidates
+
+
+int main(void)
+{
 candidate candidates[5];
 
 candidates[0].name = "Pepa";
 candidates[1].name = "Karel";
 candidates[2].name = "Honza";
 candidates[3].name = "Mirek";
-candidates[4].name = "David";
+candidates[4].name = "";
 
 candidates[0].votes = 1;
 candidates[1].votes = 2;
 candidates[2].votes = 3;
 candidates[3].votes = 4;
-candidates[4].votes = 5;
+candidates[4].votes = 0;
 
 // Number of candidates
 int candidate_count = 5;
 
-int main(void)
-{
     int lwst = 0;
      for (int j = 0; j < candidate_count - 1; j++) //swapne nejmenšího s akutálně nařadě od leva nejmenší po doprava největší
     {
@@ -38,15 +40,19 @@ int main(void)
         for (int i = 0; i < candidate_count - 1; i ++) //nalezne dalšího nejmenšího
         {
             if (candidates[lwst].votes > candidates[i + 1].votes)
+            {
+
+            }
             else
             {
                 lwst = i + 1;
             }
         }
-        candidates[j] = candidates[8];
+        candidates[j] = candidates[4];
         candidates[j] = candidates[lwst];
-        candidates[lwst] = candidates[8];
+        candidates[lwst] = candidates[4];
         lwst =+
     }
     return 0;
+    b
 }
