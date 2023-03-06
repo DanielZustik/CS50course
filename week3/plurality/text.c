dce8b656a50f0d1d3381208f36e696bbbd078860
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-06 16:06:11 +0000

    Mon, Mar 6, 2023, 5:06 PM +01:00

--- week3/plurality/text.c
+++ week3/plurality/text.c
@@ -5,8 +5,9 @@
 
 int main (void)
 {
+    int pocet = 4;
     int nejmensi = 0;
-    int neco[] = {1, 3, 4, 7}
+    int neco[] = {1, 3, 4, 7};
     for (int i = 0; i < pocet - 1; i++) //posun +1
     {
         if (neco[nejmensi] < neco[i + 1]) //aktivování a srovnání
