2e7d8ac840dffb56ca3d658e945a859927640d78
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-06 16:04:28 +0000

    Mon, Mar 6, 2023, 5:04 PM +01:00

--- week3/plurality/text.c
+++ week3/plurality/text.c
@@ -3,73 +3,20 @@
 #include <string.h>
 
 
-// Candidates have name and vote count
-typedef struct
+int main (void)
 {
-    string name;
-    int votes;
-}
-candidate;
-
-// Array of candidates
-
-
-int main(void)
-{
-
-    candidate candidates[5];
-
-    candidates[0].name = "Pepa";
-    candidates[1].name = "Karel";
-    candidates[2].name = "Honza";
-    candidates[3].name = "Mirek";
-    candidates[4].name = "";
-
-    candidates[0].votes = 1;
-    candidates[1].votes = 2;
-    candidates[2].votes = 3;
-    candidates[3].votes = 4;
-    candidates[4].votes = 0;
-
-    // Number of candidates
-    int candidate_count = 5;
-
-    int lwst = 0;
-    for (int j = 0; j < candidate_count - 1; j++) //swapne nejmenšího s akutálně nařadě od leva nejmenší po doprava největší
+    int nejmensi = 0;
+    int neco[] = {1, 3, 4, 7}
+    for (int i = 0; i < pocet - 1; i++) //posun +1
     {
-
-        for (int i = 0; i < candidate_count - 1; i ++) //nalezne dalšího nejmenšího
+        if (neco[nejmensi] < neco[i + 1]) //aktivování a srovnání
         {
-            if (candidates[lwst].votes > candidates[i + 1].votes)
-            {
-
-            }
-            else
-            {
-                lwst = i + 1;
-            }
+            nejmensi = neco[nejmensi]; //zaznamenání
         }
-        candidates[j] = candidates[4];
-        candidates[j] = candidates[lwst];
-        candidates[lwst] = candidates[4];
-        lwst =+
-    }
-    return 0;
-
-    for (int i = 0; i < počet; i++)
-    {
-        pomocná = prvek[i]
-        for (int j = 0; j < počet; j++)
+        else
         {
-            if (pomocná < prvek[j + 1])
-            {
-
-            }
-            else
-            {
-                pomocná = prvek[j + 1]
-            }
-            
+            nejmensi = neco[i + 1]; //zaznamenání
         }
     }
+    printf("nejmensi: pozice %i, hodnota %i", nejmensi, neco[nejmensi]);
 }
