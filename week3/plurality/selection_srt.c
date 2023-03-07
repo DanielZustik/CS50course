8bf1dd67fdb3b8da23b703969781913ac6045dd4
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-07 12:47:09 +0000

    Tue, Mar 7, 2023, 1:47 PM +01:00

--- week3/plurality/selection_srt.c
+++ week3/plurality/selection_srt.c
@@ -8,16 +8,21 @@ int main (void)
     int pocet = 5;
     int nejmensi = 0;
     int neco[] = {1, 3, 4, 7, 0};
-    for (int i = 0; i < pocet - 1; i++) //posun +1
+
+    for (int j = 0; j < pocet - 1; j++)
     {
-        if (neco[nejmensi] < neco[i + 1]) //aktivování a srovnání
+        for (int i = 0; i < pocet - 1; i++) //posun +1
         {
-            // nejmensi = nejmensi; //zaznamenání
-        }
-        else
-        {
-            nejmensi = i + 1; //zaznamenání
+            if (neco[nejmensi] < neco[i + 1]) //aktivování a srovnání
+            {
+                // nejmensi = nejmensi; //zaznamenání
+            }
+            else
+            {
+                nejmensi = i + 1; //zaznamenání
+            }
         }
     }
+
     printf("nejmensi: pozice %i + 1, hodnota %i\n", nejmensi, neco[nejmensi]);
 }
