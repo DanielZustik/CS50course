5115c02d3f7ba6fc6404f2e7e9e43d6dae5bc65e
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-07 13:36:18 +0000

    Tue, Mar 7, 2023, 2:36 PM +01:00

--- week3/plurality/selection_srt.c
+++ week3/plurality/selection_srt.c
@@ -12,7 +12,8 @@ int main (void)
 
     for (int j = 0; j < pocet - 1; j++)
     {
-        for (int i = pomocna; i < pocet - 1; i++) //posun +1
+        nejmensi = j; //zresetování nejmensi z posledni nalezene nejmensi na nvou possibly nejmensi zacatek loopu
+        for (int i = j; i < pocet - 1; i++) //posun +1
         {
             if (neco[nejmensi] < neco[i + 1]) //aktivování a srovnání, i jako pomocna nejprve je nula, nicméně při dalším průchodu vnější smyčkou bude 1 atd.
             {
@@ -27,7 +28,7 @@ int main (void)
         pomocna = neco[j];
         neco[j] = neco[nejmensi]; //přepis nejmensi na zacatek resp. doleva
         neco[nejmensi] = pomocna; //zápis hodnoty zleva do pozice doprava na místo nejmenší
-        pomocna = j + 1;
+        //pomocna = j + 1;
     }
     printf("pozice 0.: %i\npozice 1.: %i\npozice 2.: %i\npozice 3.: %i\npozice 4.: %i\n", neco[0], neco[1], neco[2], neco[3], neco[4]);
 }
