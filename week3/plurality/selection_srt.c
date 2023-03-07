c17322f58fe8ea199e687f6585738cd70c83588c
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-07 13:28:48 +0000

    Tue, Mar 7, 2023, 2:28 PM +01:00

--- week3/plurality/selection_srt.c
+++ week3/plurality/selection_srt.c
@@ -7,7 +7,7 @@ int main (void)
 {
     int pocet = 5;
     int nejmensi = 0;
-    int neco[] = {1, 3, 4, 7, 0};
+    int neco[] = {5, 3, 4, 8, 0};
     int pomocna = 0;
 
     for (int j = 0; j < pocet - 1; j++)
@@ -29,7 +29,5 @@ int main (void)
         neco[nejmensi] = pomocna; //zápis hodnoty zleva do pozice doprava na místo nejmenší
         //pomocna = j + 1;
     }
-
-    printf("nejmensi: pozice %i + 1, hodnota %i\n", nejmensi, neco[nejmensi]);
-    printf("pozice 0.: %i, pozice 1.: %i, pozice 2.: %i, pozice 3.: %i, pozice 4.: %i\n", neco[0], neco[1], neco[2], neco[3], neco[4]);
+    printf("pozice 0.: %i\n, pozice 1.: %i\n, pozice 2.: %i\n, pozice 3.: %i\n, pozice 4.: %i\n", neco[0], neco[1], neco[2], neco[3], neco[4]);
 }
