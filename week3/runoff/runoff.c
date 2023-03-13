5da6be71ac4251d574c0a527653b744de86632e0
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-13 13:08:53 +0000

    Mon, Mar 13, 2023, 2:08 PM +01:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -150,7 +150,10 @@ void tabulate(void)
         for (int j = 0; j < candidate_count; j++)
         {
             if (!candidates[preferences[i][j]].eliminated)
+            {
                 candidates[preferences[i][j]].votes++;
+                break;
+            }
         }
     }
     return;
