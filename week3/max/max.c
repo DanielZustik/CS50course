3b4d1cdec9b181cd00ba31d6d86909db3dc338c6
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-15 10:30:50 +0000

    Wed, Mar 15, 2023, 11:30 AM +01:00

--- week3/max/max.c
+++ week3/max/max.c
@@ -30,7 +30,7 @@ int max(int array[], int n)
 
     int max = array[0];
 
-    for (int i = 0; i < n - 1; i ++)
+    for (int i = 0; i < n; i ++)
     {
         if (array[i] > max)
             max = array[i];
