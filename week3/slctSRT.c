4db902f4d153eb0644b7db3daa83a7d90e564ee2
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-15 13:01:55 +0000

    Wed, Mar 15, 2023, 2:01 PM +01:00

--- week3/slctSRT.c
+++ week3/slctSRT.c
@@ -35,11 +35,12 @@ int main(void)
             }
         }
         max = prvky[j + 1]; //zresetování max temp na hodnotu dalšího prvku v pořadí ze začátku
-        max_i = j + 1; //zresetování max temp na hodnotu dalšího prvku v pořadí ze začátku, musí se také udělat, jinak zůstane stará pozice a nepřeuloží se, pokud se nenajde v poli už žádný větší
 
         hlp = prvky[j]; //uložení do pomocné proměnné
         prvky[j] = prvky[max_i];
         prvky[max_i] = hlp;
+
+        max_i = j + 1; //zresetování max temp na hodnotu dalšího prvku v pořadí ze začátku, musí se také udělat, jinak zůstane stará pozice a nepřeuloží se, pokud se nenajde v poli už žádný větší
     }
     for (int i = 0; i < N; i++)
         printf("prvek %i: %i\n", i, prvky[i]);
