cb34ab55973b938e3186eac45ebd1a6e2bcce4ec
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-15 17:38:29 +0000

    Wed, Mar 15, 2023, 6:38 PM +01:00

---
+++ week3/mrgSRT.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int const N = 10;
+
+int main(void)
+{
+    int prvky[N];
+
+
+    prvky[0] = 9;
+    prvky[1] = 10;
+    prvky[2] = 21;
+    prvky[3] = 14;
+    prvky[4] = 5;
+    prvky[5] = 0;
+    prvky[6] = 1;
+    prvky[7] = 6;
+    prvky[8] = 10;
+    prvky[9] = 4;
+
+    //1. sort left half
+    //2. sort right half
+    //3. merge them together
+
+    //nejprve početně, přesně mechanicky krok po bodu jen na jeden konkrétní příklad
+    //následně zobecnění a přepsání do rekurze
+
+}
