1262034c5767b0186adb85a0e5bd63b4c8be5998
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-20 11:37:51 +0000

    Mon, Mar 20, 2023, 12:37 PM +01:00

--- week4/smiley/helpers.c
+++ week4/smiley/helpers.c
@@ -3,9 +3,11 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    image[0][0].rgbtBlue = 255;
-    image[0][0].rgbtGreen = 0;
-    image[0][0].rgbtRed = 0;
+    RGBTRIPLE pixel = image[0][0];
+
+    pixel.rgbtBlue = 255;
+    pixel.rgbtGreen = 0;
+    pixel.rgbtRed = 0;
 
     return;
 }
