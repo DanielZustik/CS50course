a7ab61076c5ae8ff6e148d53d2477ac4dcd191a9
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-20 12:03:13 +0000

    Mon, Mar 20, 2023, 1:03 PM +01:00

--- week4/smiley/helpers.c
+++ week4/smiley/helpers.c
@@ -9,7 +9,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < 8; j++)
         {
-            if (image[i][j].rgbtGreen == 255 && image[i][j].rgbtRed == 255 && image[i][j].rgbtBlue == 255)
+            if (image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0 && image[i][j].rgbtBlue == 0)
             {
                 image[i][j].rgbtGreen = 55;
                 image[i][j].rgbtRed = 68;
