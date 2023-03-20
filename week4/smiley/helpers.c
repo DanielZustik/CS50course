53f2a560f248b5795675ec5c0c9ebeadcf2db67a
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-20 11:52:12 +0000

    Mon, Mar 20, 2023, 12:52 PM +01:00

--- week4/smiley/helpers.c
+++ week4/smiley/helpers.c
@@ -4,12 +4,14 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosingasd
-    printf("%i, %i", height, width);
 
     for (int i = 0; i < 8; i++)
     {
-        image[0][0].rgbtGreen = 255;
-        image[0][0].rgbtRed = 0;
-        image[0][0].rgbtBlue = 0;
+        for (int j = 0; j < 8; j++)
+        {
+            image[i][j].rgbtGreen = 255;
+            image[i][j].rgbtRed = 0;
+            image[i][j].rgbtBlue = 0;
+        }
     }
 }
