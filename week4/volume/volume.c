283bdc9817a964a1033a734deb60d67a86d1fd46
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-20 13:22:35 +0000

    Mon, Mar 20, 2023, 2:22 PM +01:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 
     for(int i = 0; i < 11; i++)
         fread(buffer, 1, 4, input);
-        fread(output, 1, 4, buffer);
+        fwrite(buffer, 1, 4, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
