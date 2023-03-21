6921481d89487c0660df2459c0005d825bd3b1a9
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-21 16:05:02 +0000

    Tue, Mar 21, 2023, 5:05 PM +01:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -21,20 +21,14 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0; i < height; i++)
         for(int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = if (round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbGreen + .131 * image[i][j].rgbtBlue) > 255)
-                image[i][j].rgbtBlue = 255;
-            else
-                image[i][j].rgbtBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbGreen + .131 * image[i][j].rgbtBlue);
+            image[i][j].rgbtBlue = (round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue) > 255) ?
+                image[i][j].rgbtBlue = 255 : image[i][j].rgbtBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
 
-            image[i][j].rgbtGreen = if (round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbGreen + .168 * image[i][j].rgbtBlue) > 255)
-                image[i][j].rgbtGreen = 255;
-            else
-                image[i][j].rgbtGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbGreen + .168 * image[i][j].rgbtBlue);
+            image[i][j].rgbtGreen = (round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue) > 255 ?
+                image[i][j].rgbtGreen = 255 : image[i][j].rgbtGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
 
-            image[i][j].rgbtRed = if (round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbGreen + .189 * image[i][j].rgbtBlue) > 255)
-                image[i][j].rgbtRed = 255;
-            else
-                image[i][j].rgbtRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbGreen + .189 * image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue) > 255 ?
+            image[i][j].rgbtRed = 255 : image[i][j].rgbtRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
         }
     return;
 }
