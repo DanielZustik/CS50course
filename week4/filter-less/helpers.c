e6d0d2afb49b5aaf4ba39d70a3e2d704f153c017
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-21 13:54:53 +0000

    Tue, Mar 21, 2023, 2:54 PM +01:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -3,6 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height - 1; i++)
+        for(int j = 0; j < width - 1; j++)
+        {
+            int x = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = x;
+            image[i][j].rgbtGreen = x;
+            image[i][j].rgbtRed = x;
+        }
+
     return;
 }
 
