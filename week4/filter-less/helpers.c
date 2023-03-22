b1e8ef3845f6ab90d159443d8eac61becccc018d
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-22 12:03:43 +0000

    Wed, Mar 22, 2023, 1:03 PM +01:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -21,14 +21,14 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0; i < height; i++)
         for(int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = (round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue) > 255) ?
+            image[i][j].rgbtBlue = (round(0.272 * image[i][j].rgbtBlue + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtRed) > 255) ?
                  255 : round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
 
-            image[i][j].rgbtGreen = (round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue) > 255) ?
+            image[i][j].rgbtGreen = (round(0.349 * image[i][j].rgbtBlue + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtRed) > 255) ?
                 255 : round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
 
-            image[i][j].rgbtRed = (round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue) > 255) ?
-            255 : round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = (round(0.393 * image[i][j].rgbtBlue + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtRed) > 255) ?
+            255 : round(0.393 * image[i][j].rgbtBLue + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtRed);
         }
     return;
 }
