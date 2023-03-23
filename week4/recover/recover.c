1b660253dd6ac7599471e228c567fcc5641465e2
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 15:46:46 +0000

    Thu, Mar 23, 2023, 4:46 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
     int file_count = 0;
     bool file_open = false;
     char filename[64];
-    FILE newfile;
+    FILE *newfile;
 
     for(int i = 0; i < 3; i++) { // konec souboru
         fread(&buffer, 512, 1, file);
         for(int j = 0; j < 512; j++)
             if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255) {
                 if (file_open)
-                    fclose(&newfile);
+                    fclose(newfile);
 
                 file_count ++; // po5adnik p5i4teni dalisoh otevrenoho filu
 
