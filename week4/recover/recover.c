be1087e7dc44c448e9a7ee22e9c4947e82862300
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 15:33:30 +0000

    Thu, Mar 23, 2023, 4:33 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+
 
 typedef uint8_t  BYTE;
 
@@ -11,17 +13,19 @@ int main(int argc, char *argv[])
 
     BYTE buffer[512];
 
-    int file_count;
+    int file_count = 0;
     bool open_file = false;
     char filename[64];
 
-    for(int i = 0; i < konec souboru; i++) {
+    for(int i = 0; i < 3; i++) { // konec souboru
         fread(&buffer, 512, 1, file);
         for(int j = 0; j < 512; j++)
             if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255) {
-                file_count +=; // po5adnik p5i4teni dalisoh otevrenoho filu
+                file_count ++; // po5adnik p5i4teni dalisoh otevrenoho filu
+
                 snprintf(filename, sizeof(filename), "%d.jpeg", file_count);
                 FILE *newfile = fopen(filename, "w");
+
                 open_file = true;
             }
             //open/close file
