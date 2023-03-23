09831789239236b82be4e3f67794621d71a6bd95
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 14:49:54 +0000

    Thu, Mar 23, 2023, 3:49 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -12,13 +12,12 @@ int main(int argc, char *argv[])
     BYTE buffer[512];
 
 
-    for(int i = 0; i < 512; i++) {
+    for(int i = 0; i < konec souboru; i++) {
         fread(&buffer, 512, 1, file);
         for(int j = 0; j < 512; j++) {
             if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255)
-                break z hledani
-        open new file
-        write
+            open/close file
+            write
     }
 
 }
