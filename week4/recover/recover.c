809859a0b18af1316197cbcd99a1e6eca3ae9cfb
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 15:51:40 +0000

    Thu, Mar 23, 2023, 4:51 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
             }
             else
                 if (file_open)
-                    write
+                    fwrite()
             //open/close file
             //write
     }
