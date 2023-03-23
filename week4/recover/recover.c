f52816e21904857423a0d160cf5d21e278656204
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 14:28:23 +0000

    Thu, Mar 23, 2023, 3:28 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -9,11 +9,13 @@ int main(int argc, char *argv[])
 
     FILE *file = fopen(argv[1], "r");
 
-    BYTE buffer[512];
+    BYTE buffer[512 * 10];
 
-    fread(&buffer, 512, 1, file);
+    fread(&buffer, 512 * 10, 1, file);
 
-    for(int i = 0; i < 512; i++)
+    for(int i = 0; i < 512 * 10; i++) {
         if (buffer[i] == 255 && buffer[i + 1] == 216 && buffer[i + 2] == 255)
             printf("%i", buffer[i]);
+        //printf("%x", buffer[i]);
+    }
 }
