b4e1a653fef439e130b3a0a9d0e2ded8c2359224
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 15:35:46 +0000

    Thu, Mar 23, 2023, 4:35 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -14,19 +14,22 @@ int main(int argc, char *argv[])
     BYTE buffer[512];
 
     int file_count = 0;
-    bool open_file = false;
+    bool file_open = false;
     char filename[64];
 
     for(int i = 0; i < 3; i++) { // konec souboru
         fread(&buffer, 512, 1, file);
         for(int j = 0; j < 512; j++)
             if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255) {
+                if (file_open)
+                    fclose(filename);
+
                 file_count ++; // po5adnik p5i4teni dalisoh otevrenoho filu
 
                 snprintf(filename, sizeof(filename), "%d.jpeg", file_count);
                 FILE *newfile = fopen(filename, "w");
 
-                open_file = true;
+                file_open = true;
             }
             //open/close file
             //write
