d552e0d0f84e060411a23193c33348dcea0c3166
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 13:06:18 +0000

    Thu, Mar 23, 2023, 2:06 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -11,10 +11,7 @@ int main(int argc, char *argv[])
 
     BYTE buffer[512];
 
-    for (int j = 0; j < 10; j++) {
-        fread(&buffer, 512, 1, file);
+    fread(&buffer, 512, 1, file);
 
-        for (int i = 0; i < 512; i++)
-            printf("%x ", buffer[i]);
-    }
+    
 }
