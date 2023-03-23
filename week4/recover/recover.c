9f1cb69a830f625da04afc50a51de8e751b26cc7
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 15:38:02 +0000

    Thu, Mar 23, 2023, 4:38 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         for(int j = 0; j < 512; j++)
             if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255) {
                 if (file_open)
-                    fclose(filename);
+                    fclose(newfile);
 
                 file_count ++; // po5adnik p5i4teni dalisoh otevrenoho filu
 
