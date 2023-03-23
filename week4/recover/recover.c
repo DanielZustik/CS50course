fa5cb82e9e01175913115e2a47a631022b340a28
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-23 14:46:35 +0000

    Thu, Mar 23, 2023, 3:46 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -9,14 +9,16 @@ int main(int argc, char *argv[])
 
     FILE *file = fopen(argv[1], "r");
 
-    BYTE buffer[1036];
+    BYTE buffer[512];
 
-    fread(&buffer, 1036, 1, file);
 
-    for(int i = 0; i < 1036; i++) {
-        if (buffer[i] == 255 && buffer[i + 1] == 216 && buffer[i + 2] == 255)
-            printf("%i ", i);
-        //printf("%x ", buffer[i]);
+    for(int i = 0; i < 512; i++) {
+        fread(&buffer, 512, 1, file);
+        for(int j = 0; j < 512; j++) {
+            if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255)
+                open new file
+                write
+                naleyeni noveho jpeg
     }
-    
+
 }
