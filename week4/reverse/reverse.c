7a55f31a93b48c7f5f091285d5ca80e47b8f0439
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-24 15:54:09 +0000

    Fri, Mar 24, 2023, 4:54 PM +01:00

--- week4/reverse/reverse.c
+++ week4/reverse/reverse.c
@@ -51,6 +51,7 @@ int main(int argc, char *argv[])
 
     // Use get_block_size to calculate size of block
     // TODO #7
+    get_block_size(header);
 
 
     // Write reversed audio to file
@@ -73,6 +74,5 @@ int check_format(WAVHEADER header)
 int get_block_size(WAVHEADER header)
 {
     // TODO #7
-    header.bitsPerSample
-    return 0;
+    return WORD block_size = header.bitsPerSample * numChannels / 8;
 }
