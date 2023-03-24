677f8dcf2c68f979b82cda15162eb0ec0db72108
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-24 15:02:43 +0000

    Fri, Mar 24, 2023, 4:02 PM +01:00

--- week4/reverse/reverse.c
+++ week4/reverse/reverse.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
-    if (argc != 3) {
+    if (argc ==! 3) {
         printf("not proper usage, use:...");
         return 1;
     }
