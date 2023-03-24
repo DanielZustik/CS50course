9736ca71dee9255b01d302c7698afbfc88c9df65
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-24 13:42:18 +0000

    Fri, Mar 24, 2023, 2:42 PM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -32,9 +32,7 @@ int main(int argc, char *argv[])
             if (buffer[j] == 255 && buffer[j + 1] == 216 && buffer[j + 2] == 255 && (buffer[j + 3] & 0xf0) == 0xe0) { //chzbi posledni 4 rozeznavaci znak jpegu
                 if (file_open)
                     fclose(newfile);
-
-                file_count ++; // po5adnik p5i4teni dalisoh otevrenoho filu
-
+                    
                 snprintf(filename, sizeof(filename), "%03d.jpg", file_count);
                 newfile = fopen(filename, "w");
 
