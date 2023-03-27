f7b8089a71eed05438418ce04a1bbedc72400a0f
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-27 10:09:17 +0000

    Mon, Mar 27, 2023, 12:09 PM +02:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 int main(void)
 {
@@ -8,10 +10,10 @@ int main(void)
 
     int l_text = strlen(text);
 
-    int w_count;
-    int s_count;
+    int w_count = 0;
+    int s_count = 0;
     for (int i = 0; i < l_text; i++) {
-        if (text[i] == " ")
+        if (text[i] == ' ')
             w_count++;
         if (text[i] == '.'|| text[i] == '?' || text[i] == '!')
             s_count++;
@@ -20,6 +22,9 @@ int main(void)
     int c_count = l_text - w_count - s_count;
 
     float L = c_count / w_count * 100;
+    float S = s_count / w_count * 100;
 
-    int index = 0.0588 * L - 0.296 *
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    printf("%i\n", index);
 }
