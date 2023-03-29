359868f2b6daaab73aaff9e80d76f2723e9d45f2
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-29 11:42:41 +0000

    Wed, Mar 29, 2023, 1:42 PM +02:00

--- week5/testovani/rectree.c
+++ week5/testovani/rectree.c
@@ -2,12 +2,12 @@
 
 bool search (node *tree, int number) // vrat yda jsi nasel, hledane cislo nubmer, a to ve stromecku zacinajici rootem tree
 {
-    if (tree == NULL)
+    if (tree == NULL) //opatreni proti prazdnemu stromku
         return false;
     else if (number < tree->number)
         return serach (tree->left, number);
     else if (number > tree->number)
         return serach (tree->right, number);
     else if (number == tree->nubmer)
-     return true;
+     return true; // nalezeno
 }
