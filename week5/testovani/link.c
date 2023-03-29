17c8378cc359f471ef80f476f65c74de79ad4e35
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-29 10:35:08 +0000

    Wed, Mar 29, 2023, 12:35 PM +02:00

--- week5/testovani/link.c
+++ week5/testovani/link.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 
-
 typedef struct node {
     int values[2];
     struct node *ptrs;
@@ -8,7 +7,6 @@ typedef struct node {
 
 int main (void)
 {
-
     node *list = NULL;
     node *n = malloc(sizeof(node));
     n -> values[0] = 1; // or (n*).
@@ -17,7 +15,7 @@ int main (void)
     list = n;
 
     //vztvoreni dalsiho prvku
-    node *n = malloc(sizeof(node));
+    n = malloc(sizeof(node));
     n -> values[0] = 2;
     n -> values[1] = 2;
     n -> ptrs = NULL;
@@ -25,4 +23,13 @@ int main (void)
     //now pointing the new one to older one, connecting it actualy
     n -> ptrs = list;
     list = n;
+
+    // vztvoreni var pointer ukazujici na list, resp ze zacatku an nic
+    // vytvoreni pomocne var pointeru n ukazujici na misto v pameti odpovidajci new prvku, nodu.. a to s pomoci malloc
+    // inicializovani hodnot v nodu skrze n
+    // napointovani list promene na tuto node resp. list
+    // vytvoreni noveho prvku, nodu skrye pomocnou n a malloc, incializovani hodnot
+    // dulezite napointovani nove promenne na starou, tj kde ukazuje list
+    // nasledne napointovani listu skrze n na novou promennou
+    // repeat
 }
