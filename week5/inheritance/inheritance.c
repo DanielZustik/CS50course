0a1f840f7893657e17c08eb26b4ba785700aa01d
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-30 11:15:29 +0000

    Thu, Mar 30, 2023, 1:15 PM +02:00

--- week5/inheritance/inheritance.c
+++ week5/inheritance/inheritance.c
@@ -54,7 +54,7 @@ person *create_family(int generations)
         p->parent[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = ;
+        p->alleles[0] = rand ();
         p->alleles[1] = ;
 
     }
