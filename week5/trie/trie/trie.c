8d065161454581a2f99e59433038e6ba8b982c48
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-31 11:53:26 +0000

    Fri, Mar 31, 2023, 1:53 PM +02:00

--- week5/trie/trie/trie.c
+++ week5/trie/trie/trie.c
@@ -121,8 +121,10 @@ bool check(char* word)
     else
         return false
     // recursie case
-    a = *word
-    node * p = root -> children[a]
+    int a = atoi(tolower(*word)) - 97; // jdi na adresu wordu, tento char preved na lowercase, tuto hodnotu tam preved na integer a odecti 97, tim se ziska poradi v abecede
+    node * p = root -> children[a] // toto poradi odpovida abecede v nodu, a tak se na to okdaz a nalezeni adresu noveho nodu
+    //tohle se bude rekurzivne opakovat pro vsechny pismena wordu
+
     check (word + 1)
 
     return false;
