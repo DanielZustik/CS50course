9dab65a9cfffcc3acbdf7a69d5cf101c2fe65c70
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-03-31 12:27:43 +0000

    Fri, Mar 31, 2023, 2:27 PM +02:00

--- week5/trie/trie/trie.c
+++ week5/trie/trie/trie.c
@@ -121,9 +121,7 @@ bool check(char* word)
     int a = tolower(word[0]) - 97; // jdi na adresu wordu, tento char preved na lowercase, tuto hodnotu tam preved na integer a odecti 97, tim se ziska poradi v abecede
     root = root->children[a]; // toto poradi odpovida abecede v nodu, a tak se na to okdaz a nalezeni adresu noveho nodu
 
-    check(word + 1);
-
-    return;
+    return check(word + 1);
 }
 
 // Unload trie from memory
