fc6c57b6a24e3fd558f2fd1369efb0f8a7a94390
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-12 14:09:22 +0000

    Wed, Apr 12, 2023, 4:09 PM +02:00

--- week6/jar/jar.py
+++ week6/jar/jar.py
@@ -1,7 +1,8 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
-
+        if capacity < 0:
+            raise ValueError('A very specific bad thing happened.')
     def __str__(self):
         ...
 
