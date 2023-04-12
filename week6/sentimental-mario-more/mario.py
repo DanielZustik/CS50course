eb2c383c656c2a70b5da83190ea68c6a845e5754
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-12 16:48:29 +0000

    Wed, Apr 12, 2023, 6:48 PM +02:00

--- week6/sentimental-mario-more/mario.py
+++ week6/sentimental-mario-more/mario.py
@@ -2,3 +2,8 @@ while (True):
     h = int(input("enter half s pyramid height: "))
     if h in [1, 2, 3, 4, 5, 6, 7, 8]:
         break
+
+for _ in range(h):
+    print("#")
+    for _ in range(h):
+        print("#")
