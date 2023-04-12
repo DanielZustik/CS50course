60222be6fad5498e1d6948074cd582784e7c2f99
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-12 11:13:53 +0000

    Wed, Apr 12, 2023, 1:13 PM +02:00

--- week6/figlet/figlet.py
+++ week6/figlet/figlet.py
@@ -1,6 +1,11 @@
 import sys
+import pyfiglet
 
-if len(sys.argv) != 1 or len(sys.argv) != 3
-    print
+if len(sys.argv) != 1 or len(sys.argv) != 3:
+    print("porper usage...")
     sys.exit
+elif len(sys.argv) == 1:
 
+elif len(sys.argv) == 3:
+
+s = input("insert ur text")
