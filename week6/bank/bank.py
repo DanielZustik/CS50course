a2f1e9f172be0af690854850bf88ee2edd0e0265
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-12 10:58:31 +0000

    Wed, Apr 12, 2023, 12:58 PM +02:00

--- week6/bank/bank.py
+++ week6/bank/bank.py
@@ -1,7 +1,7 @@
 s = input("Hello, sir! <greet back:>")
 s = s.lower().strip()
 
-if "hello" in s and :
+if "hello" in s and s.find("hello") == 0:
     print("0 $")
 elif s[0] == "h":
     print("20 $")
