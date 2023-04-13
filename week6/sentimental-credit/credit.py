5289cd5ed98b6e40d1f34558476226b1de23b472
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-13 10:58:42 +0000

    Thu, Apr 13, 2023, 12:58 PM +02:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -1,21 +1,33 @@
+import sys
+
 c = input("Credit card number: ")
 
-for d in range(len(c) - 2, -1, -2):
-    sum += int(c[d]) * 2
-for g in range(len(c) - 1, -1, -2):
-    sum_ += int(c[g])
-check = sum + sum_
-check = str(check)
+def checksum():
+    for d in range(len(c) - 2, -1, -2):
+        sum += int(c[d]) * 2
+    for g in range(len(c) - 1, -1, -2):
+        sum_ += int(c[g])
+    check = sum + sum_
+    check = str(check)
 
-if check[len(check) - 1:] == 0:
-    if c [0] == '4' and len(c) == 13 or len(c) == 16:
-        print("VISA")
-    elif c [:2] in ['34', '37'] and len(c) == 15:
-        print("AMEX")
-    elif c [:2] in ['51', '52', '53', '54', '55'] and len(c) == 16:
-        print("MASTERCARD")
+    if check[len(check) - 1:] == 0:
+        return  True
     else:
         print("INVALID")
+        sys.exit
+
+if c [0] == '4' and len(c) == 13 or len(c) == 16:
+    checksum()
+    print("VISA")
+elif c [:2] in ['34', '37'] and len(c) == 15:
+    checksum()
+    print("AMEX")
+elif c [:2] in ['51', '52', '53', '54', '55'] and len(c) == 16:
+    checksum()
+    print("MASTERCARD")
 else:
+    checksum()
     print("INVALID")
+
+
 
