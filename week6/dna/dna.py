fbed35edd33aad0f7860bf2610119775a49e62e6
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-13 14:07:25 +0000

    Thu, Apr 13, 2023, 4:07 PM +02:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -15,9 +15,13 @@ def main():
         STR = []
         for row in dictcsv:
             STR.append(row)
-        print(STR)
 
     # TODO: Read DNA sequence file into a variable
+    with open(sys.argv[2], "r") as dna_f:
+        dictdna = csv.DictReader(dna_f)
+        DNA = []
+        for row in dictdna:
+            DNA.append(row)
 
     # TODO: Find longest match of each STR in DNA sequence
 
