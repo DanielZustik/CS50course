07a9fff3b0db9e618d8cc8da5ed3a58f3b063baa
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-13 13:03:36 +0000

    Thu, Apr 13, 2023, 3:03 PM +02:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -12,7 +12,8 @@ def main():
     # TODO: Read database file into a variable
     with open(sys.argv[1], "r") as csv_f:
         dictcsv = csv.DictReader(csv_f)
-        print(dictcsv)
+        text = dictcsv.reader()
+        print(text)
 
     # TODO: Read DNA sequence file into a variable
 
