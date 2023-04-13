08328116bf4b863a1a6647ed786b103e776990fd
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-13 11:28:29 +0000

    Thu, Apr 13, 2023, 1:28 PM +02:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -7,7 +7,7 @@ def checksum():
     sum_ = 0
     for d in range(len(c) - 2, -1, -2):
         if int(c[d]) * 2 => 10
-            ...
+            
         else:
             sum += int(c[d]) * 2
     for g in range(len(c) - 1, -1, -2):
