116e3a96e615e8482bd7b7e20fac1a29c667c358
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-13 10:41:11 +0000

    Thu, Apr 13, 2023, 12:41 PM +02:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -6,14 +6,16 @@ for d in c
     sum_ += int(d)
 check = sum + sum_
 check = str(check)
-if check[:len(check) - 1]
 
-if c [0] == '4' and len(c) == 13 or len(c) == 16:
-    print("VISA")
-elif c [:2] in ['34', '37'] and len(c) == 15:
-    print("AMEX")
-elif c [:2] in ['51', '52', '53', '54', '55'] and len(c) == 16:
-    print("MASTERCARD")
+if check[len(check) - 1:] == 0
+    if c [0] == '4' and len(c) == 13 or len(c) == 16:
+        print("VISA")
+    elif c [:2] in ['34', '37'] and len(c) == 15:
+        print("AMEX")
+    elif c [:2] in ['51', '52', '53', '54', '55'] and len(c) == 16:
+        print("MASTERCARD")
+    else
+        print("INVALID")
 else
     print("INVALID")
 
