48d6452e80636037943c9718e89942c093fcacb6
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-13 14:29:59 +0000

    Thu, Apr 13, 2023, 4:29 PM +02:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -21,7 +21,10 @@ def main():
         DNA = dna_f.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    keys_l = []
+    keys_l = STR[0]
     for key:1 in STR[0]:
+        keys_l.
         longest_match(DNA, [key])
         print(key)
 
