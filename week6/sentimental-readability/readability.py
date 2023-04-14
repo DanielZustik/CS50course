09823bdfdece0464302f89a11c56f230c7a79908
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-14 17:11:21 +0000

    Fri, Apr 14, 2023, 7:11 PM +02:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -5,13 +5,13 @@ def main():
     text = input("Text: ")
 
     counts = counting(text)
-    grade = ...
-    if grade < 1:
-        print()
-    elif grade <16:
-        print()
-    else:
-        print()
+    #grade = ...
+    #if grade < 1:
+    #    print()
+    #elif grade <16:
+    #    print()
+    #else:
+    #    print()
 
 
 def counting(text):
