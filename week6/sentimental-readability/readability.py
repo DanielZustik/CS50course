61dea30e7fcccacee8b4e58298b58bf22f8b0f07
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-16 12:07:33 +0000

    Sun, Apr 16, 2023, 2:07 PM +02:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -3,8 +3,9 @@ def main():
     text = input("Text: ")
 
     counts = counting(text)
-    index = 0.0588 * (counts["letters"] / counts["words"] * 100) * (counts["letters"] / counts["words"] * 100)
-
+    index = 0.0588 * (counts["letters"] / counts["words"] * 100) - 0.296 * (counts["senteces"] / counts["words"] * 100) - 15.8
+    print(index)
+    
     #grade = ...
     #if grade < 1:
     #    print()
