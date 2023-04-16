630f2ac76c209f2c04374b14964c85a61f2ee24c
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-16 12:28:01 +0000

    Sun, Apr 16, 2023, 2:28 PM +02:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -3,7 +3,8 @@ def main():
     text = input("Text: ")
 
     counts = counting(text)
-    index = round(0.0588 * (counts["letters"] / counts["words"] * 100) - 0.296 * (counts["senteces"] / counts["words"] * 100) - 15.8)
+    index = round(0.0588 * (counts["letters"] / counts["words"] * 100) -
+                  0.296 * (counts["senteces"] / counts["words"] * 100) - 15.8)
 
     if index < 1:
         print("Before Grade 1")
@@ -21,13 +22,14 @@ def counting(text):
             letters += 1
         if c >= "A" and c <= "Z":
             letters += 1
-    words = len(text.split()) #
+    words = len(text.split())
     senteces = 0
 
     for c in text:
         if c == "." or c == "!" or c == "?":
             senteces += 1
 
-    return {"letters" : letters, "words" : words, "senteces" : senteces}
+    return {"letters": letters, "words": words, "senteces": senteces}
+
 
 main()
