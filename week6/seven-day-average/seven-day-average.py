da7c6f4c412580ea014843db6ae6a6ad2b3f953b
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-17 17:45:32 +0000

    Mon, Apr 17, 2023, 7:45 PM +02:00

--- week6/seven-day-average/seven-day-average.py
+++ week6/seven-day-average/seven-day-average.py
@@ -50,12 +50,8 @@ def calculate(reader):
     for dict in data:
         cases[dict["state"]].append(dict["cases"])
 
-    #for key in cases:
-    #    print(key)
-    #    print(cases[key][-10:])
-
-    for key in cases:
-        new_cases[key] = cases[key][-10:]
+    for key in cases: #dictionary kde key je state a u kazdeho je list s 10 poslednimi stavy
+        new_cases[key] = cases[key][-14:]
     #    print(f"{key}: {new_cases[key]}")
 
 # TODO: Calculate and print out seven day average for given state
