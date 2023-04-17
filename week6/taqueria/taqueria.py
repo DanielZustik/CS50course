b1d03cc503ab0af8a267432f436b763f8d30d59a
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-17 12:41:22 +0000

    Mon, Apr 17, 2023, 2:41 PM +02:00

--- week6/taqueria/taqueria.py
+++ week6/taqueria/taqueria.py
@@ -1,20 +1,20 @@
 menu = {
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
+    "baja taco": 4.00,
+    "burrito": 7.50,
+    "bowl": 8.50,
+    "nachos": 11.00,
+    "quesadilla": 8.50,
+    "super burrito": 8.50,
+    "super quesadilla": 9.50,
+    "taco": 3.00,
+    "tortilla salad": 8.00
 }
 
 cost = 0
 while True:
     try:
         item = input("Item: ")
-        item = item.capitalize()
+        item = item.lower()
         cost += menu[item]
         print(f"Total: {cost}")
     except EOFError:
