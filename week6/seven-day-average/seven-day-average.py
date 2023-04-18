385c4e00910f4ea49462a90e6b5384739b185a5d
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-18 09:20:46 +0000

    Tue, Apr 18, 2023, 11:20 AM +02:00

--- week6/seven-day-average/seven-day-average.py
+++ week6/seven-day-average/seven-day-average.py
@@ -64,7 +64,9 @@ def calculate(reader):
 
 # TODO: Calculate and print out seven day average for given state
 def comparative_averages(new_cases, states):
-    ...
+
+    
+    sum(new_cases[key][i]
 
 
 main()
