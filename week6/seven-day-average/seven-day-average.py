52e875c249da7cd243b4ebe7200f81aef1eab048
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-18 09:10:41 +0000

    Tue, Apr 18, 2023, 11:10 AM +02:00

--- week6/seven-day-average/seven-day-average.py
+++ week6/seven-day-average/seven-day-average.py
@@ -56,8 +56,8 @@ def calculate(reader):
     for key in new_cases:
         new_cases[key] = [int(case_count) for case_count in new_cases[key]]
 
-    for key in new_cases
-        for i in range(15, 0, -1)
+    for key in new_cases:
+        for i in range(15, 0, -1):
             new_cases[key][i] = new_cases[key][i] - new_cases[key][i - 1]
 
 # TODO: Calculate and print out seven day average for given state
