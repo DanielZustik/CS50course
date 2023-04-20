238a77abb52a83571616a87890f44f8ed0186346
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 11:22:00 +0000

    Thu, Apr 20, 2023, 1:22 PM +02:00

--- week7/prophecy/transef.py
+++ week7/prophecy/transef.py
@@ -1,11 +1,14 @@
 from cs50 import SQL
 import csv
 
+db = SQL("sqlite:///newww.db")
+
 with open('students.csv', "r") as csv_f:
     dictcsv = csv.DictReader(csv_f)
     STR = []
     for row in dictcsv:
         STR.append(row)
+    for dict in STR
+        values = 
+        db.execute("INSERT INTO students (student_name, house_id) VALUES (?)", values)
 
-db = SQL("sqlite:///newww.db")
-db.execute("")
