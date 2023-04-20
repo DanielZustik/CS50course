c93c084f6b16eee6496a3a5a32ca5e5fc5df05d9
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 14:05:34 +0000

    Thu, Apr 20, 2023, 4:05 PM +02:00

---
+++ week7/movies/13.sql
@@ -0,0 +1,4 @@
+SELECT name FROM people
+JOIN stars on stars.person_id = people.id
+JOIN movies ON stars.movie_id = movies.id
+WHERE stars.person_id = (SELECT id FROM people WHERE name = "Kevin Bacon")
