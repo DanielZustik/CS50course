5b3ba2b0d9ecbb58bff4a82bc290f3752a160e90
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 14:09:33 +0000

    Thu, Apr 20, 2023, 4:09 PM +02:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,4 +1,7 @@
-SELECT name FROM people
+SELECT DISTINCT name FROM people
 JOIN stars on stars.person_id = people.id
 JOIN movies ON stars.movie_id = movies.id
-WHERE stars.person_id = (SELECT id FROM people WHERE name = "Kevin Bacon");
+WHERE stars.movie_id =
+    (SELECT id FROM people
+        WHERE name = "Kevin Bacon"
+        WHERE birth = 1958)
