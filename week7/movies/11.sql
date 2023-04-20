2e050c3ec91ff5f489af329195012a335fefb77f
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 14:14:50 +0000

    Thu, Apr 20, 2023, 4:14 PM +02:00

--- week7/movies/11.sql
+++ week7/movies/11.sql
@@ -3,4 +3,5 @@ JOIN ratings ON movies.id = ratings.movie_id
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = "Chadwick Boseman"
-ORDER BY ratings.rating DESC;
+ORDER BY ratings.rating DESC
+LIMIT 5;
