8280fe0a58c6ffdc4559fd424adebea23715e195
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 13:25:32 +0000

    Thu, Apr 20, 2023, 3:25 PM +02:00

--- week7/movies/9.sql
+++ week7/movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2004 ORDER BY people.birth;
+SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2004 ORDER BY people.birth;
