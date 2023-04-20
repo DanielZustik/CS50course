fdb7af4776ae3cd42fefa6dfa82f7b9df0b3d83b
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 14:15:41 +0000

    Thu, Apr 20, 2023, 4:15 PM +02:00

--- week7/movies/4.sql
+++ week7/movies/4.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating = 10;
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating = 10.0;
