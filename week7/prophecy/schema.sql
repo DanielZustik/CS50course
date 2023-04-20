030a1e8a5bf712934128a60c1c4d7082cedefec1
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-20 10:52:41 +0000

    Thu, Apr 20, 2023, 12:52 PM +02:00

--- week7/prophecy/schema.sql
+++ week7/prophecy/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE students (
     id INTEGER,
     student_name TEXT,
+    house_id INTEGER,
     PRIMARY KEY(id)
+    FOREIGN KEY(house_id) REFERENCES houses(id)
 );
 CREATE TABLE houses (
     id INTEGER,
     house_name TEXT,
-    student_id INTEGER,
     PRIMARY KEY(id),
-    FOREIGN KEY(student_id) REFERENCES students(id)
 );
 CREATE TABLE heads (
     id INTEGER,
