ac1424e6a792230eae963e8863400312bd79f9ae
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-21 13:33:12 +0000

    Fri, Apr 21, 2023, 3:33 PM +02:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -2,4 +2,6 @@
 SELECT * from <tables> -- to see which column tables consists of... or which information they are holding. findind conecting columns betwen tables too
 SELECT * from crime_scene_report; -- seeing more info into case, conections
 .schema interviews -- to see have can i trace those intervies
-
+SELECT name FROM interviews WHERE name LIKE "%CS50%"; -- hopefully finding cs50 interview
+SELECT name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
