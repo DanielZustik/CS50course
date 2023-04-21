ea3e6ce66d42a78b9e0bdcc2ac855f9a84765d1f
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-21 13:27:18 +0000

    Fri, Apr 21, 2023, 3:27 PM +02:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * from <tables> -- to see which column tables consists of... or which information they are holding. findind conecting columns betwen tables too
 SELECT * from crime_scene_report; -- seeing more info into case, conections
-
+.schema interviews -- to see have can i trace those intervies
 
