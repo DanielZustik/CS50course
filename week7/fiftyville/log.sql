c32ea65127601ea073068e3a38b00be310b9c3b5
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-04-23 12:35:27 +0000

    Sun, Apr 23, 2023, 2:35 PM +02:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -33,5 +33,6 @@ SELECT * FROM airports WHERE id = 6;
 SELECT * FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.person_id
-WHERE atm_transactions.year = 2021 AND atm_transactions.month = 7 AND atm_transactions.day = 28 AND atm_transactions.atm_location = "Leggett Street" and atm_transactions.transaction_type = "withdraw";
+WHERE atm_transactions.year = 2021 AND atm_transactions.month = 7 AND atm_transactions.day = 28
+AND atm_transactions.atm_location = "Leggett Street" and atm_transactions.transaction_type = "withdraw";
 SELECT * FROM flights WHERE origin_airport_id = (SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29 AND month = 7;
