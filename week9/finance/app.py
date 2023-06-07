421a4e9668d207b61da793f03f0ed80aed4406aa
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-06-07 14:43:04 +0000

    Wed, Jun 7, 2023, 4:43 PM +02:00

--- week9/finance/app.py
+++ week9/finance/app.py
@@ -104,7 +104,14 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        
+
+        return render_template("quoted.html")
+
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
