8999f373ec781984abdc44c5f7711e5d6b3e567a
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-06-08 14:30:23 +0000

    Thu, Jun 8, 2023, 4:30 PM +02:00

--- week9/finance/app.py
+++ week9/finance/app.py
@@ -37,9 +37,8 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    portfolio = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol", session["user_id"])
-    shares = db.execute("SELECT SUM shares FROM transactions WHERE user_id = ? GROUP BY symbol", session["user_id"])
-    return render_template("index.html", portfolio=portfolio, shares=shares)
+    portfolio = db.execute("SELECT symbol, SUM(shares) as total_shares FROM transactions WHERE user_id = ? GROUP BY symbol", session["user_id"])
+    return render_template("index.html", portfolio=portfolio)
 
 
 @app.route("/buy", methods=["GET", "POST"])
