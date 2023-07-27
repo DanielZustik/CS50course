9caf0ad62945254ff7689460d16b99fff1aecb93
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-27 12:55:57 +0000

    Thu, Jul 27, 2023, 2:55 PM +02:00

--- CS50P/seasons/seasons.py
+++ CS50P/seasons/seasons.py
@@ -3,11 +3,14 @@ import sys
 import inflect
 
 def main():
+    print(inflect.engine().number_to_words(minutes_elapsed(birthday), andword="").capitalize(), "minutes")
+
+
+def get_birth():
     try:
         birthday = date.fromisoformat(input("type date of ur birth in YYYY-MM-DD: "))
     except ValueError:
         sys.exit(1)
-    print(inflect.engine().number_to_words(minutes_elapsed(birthday), andword="").capitalize(), "minutes")
 
 
 def minutes_elapsed(birthday):
