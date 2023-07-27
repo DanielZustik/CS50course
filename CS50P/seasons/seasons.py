4140b54387d4d4f916447ef320f9ab3686ecd4bc
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-27 11:26:32 +0000

    Thu, Jul 27, 2023, 1:26 PM +02:00

--- CS50P/seasons/seasons.py
+++ CS50P/seasons/seasons.py
@@ -4,6 +4,11 @@ from datetime import date
 def main():
     print(date.today()) #returns current local date
     print(date(2029,1,1))
+    today = date.today()
+    birthday = date(2000,1,1)
+
+    minutes = today - birthday
+    print(minutes.mins)
 
 
 
