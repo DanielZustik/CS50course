a85bdd48d1cd7ae9b0ce0df56d33f320c710352e
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-27 12:10:29 +0000

    Thu, Jul 27, 2023, 2:10 PM +02:00

--- CS50P/seasons/seasons.py
+++ CS50P/seasons/seasons.py
@@ -5,6 +5,8 @@ import sys
 def main():
     try:
         birthday = date.fromisoformat(input("type date of ur birth in YYYY-MM-DD: "))
+        print(birthday)
+        print(date.today())
     except ValueError:
         sys.exit()
     print("u are ", minutes_elapsed(birthday), "minutes old")
