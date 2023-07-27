91bbd1b12987ded0ca793f7ffe56fb53c2c40c4b
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-27 13:03:36 +0000

    Thu, Jul 27, 2023, 3:03 PM +02:00

--- CS50P/seasons/test_seasons.py
+++ CS50P/seasons/test_seasons.py
@@ -1,11 +1,18 @@
 from seasons import minutes_elapsed
+from seasons import get_birth
 
 def main():
-    test_elapsed_time()
+    test_get_birth()
+    test_minutes_elapsed()
 
 
-def test_elapsed_time():
-    if 
+
+def test_get_birth():
+    if get_birth()
+
+
+def test_minutes_elapsed():
+    if
 
 
 if __name__ = "__main__":
