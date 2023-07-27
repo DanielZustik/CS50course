c0491cb43142cb03cf81bb130854a6910e77f611
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-27 11:40:26 +0000

    Thu, Jul 27, 2023, 1:40 PM +02:00

--- CS50P/seasons/seasons.py
+++ CS50P/seasons/seasons.py
@@ -2,6 +2,7 @@ from datetime import date
 
 
 def main():
+    
     birthday = date(2000,1,1)
     print(minutes_elapsed(birthday))
 
@@ -9,7 +10,7 @@ def main():
 def minutes_elapsed(birthday):
     today = date.today()
     days = today - birthday
-    return days * 24 * 60
+    return (days * 24 * 60).days
 
 
 if __name__ == "__main__":
