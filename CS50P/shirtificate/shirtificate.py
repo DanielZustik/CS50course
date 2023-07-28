e4714e82b1b8f32924554de2ba4b51467076bb4d
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-28 11:47:07 +0000

    Fri, Jul 28, 2023, 1:47 PM +02:00

--- CS50P/shirtificate/shirtificate.py
+++ CS50P/shirtificate/shirtificate.py
@@ -10,8 +10,8 @@ def create_pdf(name):
     pdf = fpdf.FPDF()
     pdf.add_page()
     pdf.set_font("helvetica", "B", 16)
-    pdf.cell(10,10, "CS50 Shirtificate", align='C')
-    pdf.cell(10,10, f"{name} took CS50", align='C')
+    pdf.cell(200,100, "CS50 Shirtificate", align='C',new_x="LMARGIN", new_y="NEXT")
+    pdf.cell(200,100, f"{name} took CS50", align='C')
     pdf.output("shirtificate.pdf")
 
 
