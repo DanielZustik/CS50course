0fd035e3e9b3b84dc2f36a0ab8e93dcab1b2807c
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-28 11:08:26 +0000

    Fri, Jul 28, 2023, 1:08 PM +02:00

--- CS50P/shirtificate/shirtificate.py
+++ CS50P/shirtificate/shirtificate.py
@@ -3,6 +3,16 @@ import fpdf
 
 def main():
     name = input("What is ur name?")
+    create_pdf(name)
+
+
+def create_pdf(name):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("helvetica", "B", 16)
+    pdf.cell(50,10, "CS50 Shirtificate")
+    pdf.cell(50,20, "Daniel Zustik took CS50")
+    pdf.output("shirtificate.pdf")
 
 
 if __name__ == "__main__":
