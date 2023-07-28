5ff99fb3ee644c246e483e7cac020a978938d831
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-28 09:13:44 +0000

    Fri, Jul 28, 2023, 11:13 AM +02:00

---
+++ CS50P/jar/jar.py
@@ -0,0 +1,24 @@
+class Jar:
+    def __init__(self, capacity = 12):
+        ...
+
+
+    def __str__(self):
+        ...
+
+
+    def deposit(n):
+        ...
+
+
+    def withdraw(n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
+
