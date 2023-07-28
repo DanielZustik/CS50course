374dad6fd0d49ea429a5ec534e6b684e0ab4c384
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-28 10:14:07 +0000

    Fri, Jul 28, 2023, 12:14 PM +02:00

--- CS50P/jar/jar.py
+++ CS50P/jar/jar.py
@@ -1,7 +1,7 @@
 class Jar:
     def __init__(self, capacity = 12):
         self.capacity = capacity
-        self.size = 0
+        self._size = 0 
 
     def __str__(self):
         return '🍪' * self.size
@@ -37,5 +37,7 @@ class Jar:
 
 
     @size.setter
-    def size(self):
-        self._size = 0
+    def size(self, size):  # The setter should take an additional argument
+        if size < 0:
+            raise ValueError("size cannot be negative")
+        self._size = size
