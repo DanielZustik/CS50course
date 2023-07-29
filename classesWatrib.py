67c72dd92ca8192aba700387f1d974f05325788b
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-29 15:35:52 +0000

    Sat, Jul 29, 2023, 5:35 PM +02:00

--- classesWatrib.py
+++ classesWatrib.py
@@ -1,25 +1,31 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("missing name")
         if house not in ["Pr", "Kac", "Sra"]:
             raise ValueError("not correct house")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
 
     def __str__(self):
-        return f"a student with name {self.name} and house {self.house}"
+        return f"a student with name {self.name} and house {self.house} with patronus {self.patronus}"
 
 
+    def charm():
+        ...
+
+#####################| MAIN |############################
 def main():
     name = input("Name:" )
     house = input("House:" )
+    patronus = input("Patronus:" )
     try:
-        student = Student(name, house)
+        student = Student(name, house, patronus)
         print(student)
     except ValueError:
         print("creating of a student failed")
 
-#####################KONEC###########################
+#####################| KONEC |###########################
 main()
