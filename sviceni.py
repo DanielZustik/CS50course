1226ae999c665a788312e942a4e596cc66262e3c
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-29 15:17:54 +0000

    Sat, Jul 29, 2023, 5:17 PM +02:00

--- sviceni.py
+++ sviceni.py
@@ -1,16 +1,20 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("")
+        if not house:
+            raise ValueError("")
         self.name = name
         self.house = house
 
 
 def main():
-    name = input("Name:" )
     house = ["Prdelov", "Kacerov", "Srandov"]
+    name = input("Name:" )
     student = Student(name, house[0])
 
     print(student.name)
     print(student.house)
 
-
+#####################KONEC###########################
 main()
