84aeca92465e2ddebb77c11ad4e5d5574b9674b8
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-30 15:44:21 +0000

    Sun, Jul 30, 2023, 5:44 PM +02:00

---
+++ inheritance.py
@@ -0,0 +1,32 @@
+##redundunace, ne takhle##
+
+#class Student:
+ #   def __init__(self, name, house):
+#        self.name = name
+#        self.house = house
+#
+#
+#class Professor:
+#    def __init__(self, name, subject):
+#        self.name = name
+ #       self.subject = subject
+########################################
+########### | Inheritance |#############
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError
+        self.name = name
+
+
+class Professor:
+    def __init__(self, subject):
+        if not subject:
+            raise ValueError
+        self.subject = subject
+
+class Student:
+    def __init__(self, house):
+        if not house:
+            raise ValueError
+        self.house = house
