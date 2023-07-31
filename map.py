3519c2e0dba9cc74639c4177d0a01b874b2c100b
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 15:41:40 +0000

    Mon, Jul 31, 2023, 5:41 PM +02:00

--- map.py
+++ map.py
@@ -1,7 +1,20 @@
+"""
 def main():
     yell("This is CS50!")
 
 def yell(words):
     print(phrase.upper())
+
+main()
+"""
+
+def main():
+    yell(["This", "is", "CS50!"])
+
+def yell(words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
 
 main()
