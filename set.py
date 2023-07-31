8b79a1408daf33179cab3bbe45505dc99a6eee68
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 09:50:34 +0000

    Mon, Jul 31, 2023, 11:50 AM +02:00

---
+++ set.py
@@ -0,0 +1,13 @@
+students = [
+    {"name": "Harry", "house": "Gryffiondor"},
+    {"name": "Hermione", "house": "Gryffiondor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+    {"name": "Ron", "house": "Gryffiondor"},
+]
+
+houses = set()
+for student in students:
+    houses.add(student["house"])
+
+print(houses)
