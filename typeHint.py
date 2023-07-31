c0bb1680a914464ca32d8c7d7630919e8d8c5f78
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 11:05:32 +0000

    Mon, Jul 31, 2023, 1:05 PM +02:00

--- typeHint.py
+++ typeHint.py
@@ -1,6 +1,12 @@
+#python doesnt support type hint doesnt care but with mypy its possible to check
 def meow(n: int):
     for _ in range(n):
         print("meow")
 
 
-meow("asd")
+number: int = "asd" #try mypy typeHint.py... btw :int is type hint
+try:
+    meow(number)
+except:
+    ...
+meow(3)
