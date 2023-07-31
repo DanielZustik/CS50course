ef428461d156935aef143b52eba50709ead017b9
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 10:17:03 +0000

    Mon, Jul 31, 2023, 12:17 PM +02:00

--- globalVar.py
+++ globalVar.py
@@ -1,21 +1,31 @@
-balance = 0
+#balance = 0
 
-def main():
-    print("Balance:", balance)
-    deposit(100)
-    withdraw(50)
-    print("Balance:", balance)
-
-
-def deposit(n):
-    global balance
-    balance += n
+#def main():
+#    print("Balance:", balance)
+#    deposit(100)
+#    withdraw(50)
+#    print("Balance:", balance)
 
 
-def withdraw(n):
-    global balance
-    balance -= n
+#def deposit(n):
+#    global balance
+#    balance += n
 
 
-if __name__ == "__main__":
-    main()
+#def withdraw(n):
+#    global balance
+#    balance -= n
+
+
+#if __name__ == "__main__":
+#    main()
+
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    
