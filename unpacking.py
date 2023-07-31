08d9b0c0c571b971d553882b57017ef2eb59574a
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 13:57:27 +0000

    Mon, Jul 31, 2023, 3:57 PM +02:00

--- unpacking.py
+++ unpacking.py
@@ -1,2 +1,7 @@
-first, last = input("what s ur full name?").split(" ")
-print(f"hello, {first}")
+#first, last = input("what s ur full name?").split(" ")
+#print(f"hello, {first}")
+
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+print(total(100, 50, 25), "Knuts")
