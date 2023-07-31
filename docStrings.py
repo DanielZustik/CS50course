b646980d0c5db471f36c128339bb679a888f5cf2
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 11:44:43 +0000

    Mon, Jul 31, 2023, 1:44 PM +02:00

--- docStrings.py
+++ docStrings.py
@@ -1,3 +1,14 @@
 def meow(n):
     """Meow n times.""" #documentation
-    return "meow"
+    return "meow" * n
+
+def bark(n):
+    """
+    Barks n times.
+
+    :param n: Number of times to bark
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of barks per line
+    """ #documentation
+    return "Bark" * n
