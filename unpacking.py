7307aefcb8a25065cbf07d08119554b6fcb19d18
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 14:43:54 +0000

    Mon, Jul 31, 2023, 4:43 PM +02:00

--- unpacking.py
+++ unpacking.py
@@ -11,9 +11,11 @@ coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 #print(total(coins["galleons"], coins["knuts"], coins["sickles"]), "Knuts")
 print(total(**coins), "Knuts") #unpacking keys and values... returing them. not just values as unpacking list.. but still works because above ..knuts=25, sickles=50)
 """
-
+"""
 def f(*args, **kwargs): #varialbe number of arguments
     print("Positional arguments:", args)
     print("Named arguments:", kwargs)
 
 f(100, value=100)
+"""
+def f(*)
