ea7b3b0f93f489b1b4a2a429e9c02462d3145f83
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 10:40:33 +0000

    Mon, Jul 31, 2023, 12:40 PM +02:00

--- constants.py
+++ constants.py
@@ -1,6 +1,16 @@
 ### not enforced by python, honor system.. capitalized name
+#MEOWS = 3
+#for _ in range(MEOWS):
+#    print("meow")
 
-MEOWS = 3
 
-for _ in range(MEOWS):
-    print("meow")
+class Cat:
+    MEOWS = 3
+
+    def meow(self):
+        for _ in range(Cat.MEOWS):
+            print("meows")
+
+
+cat = Cat()
+cat.meow()
