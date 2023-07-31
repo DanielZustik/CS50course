8494ef9ef4b5910b59fa9369cdd7924396b2c7dd
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-07-31 14:36:14 +0000

    Mon, Jul 31, 2023, 4:36 PM +02:00

--- unpacking.py
+++ unpacking.py
@@ -1,6 +1,6 @@
 #first, last = input("what s ur full name?").split(" ")
-#print(f"hello, {first}")
-
+#print(f"hello, {first}")"
+"""
 def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
@@ -10,3 +10,9 @@ def total(galleons, sickles, knuts):
 coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 #print(total(coins["galleons"], coins["knuts"], coins["sickles"]), "Knuts")
 print(total(**coins), "Knuts") #unpacking keys and values... returing them. not just values as unpacking list.. but still works because above ..knuts=25, sickles=50)
+"""
+
+def f(*args, **kwargs): #varialbe number of arguments
+    print("positional arguments:", args)
+
+f()
