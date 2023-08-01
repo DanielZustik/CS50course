d5d29d17ef74cbd74e41a102c814d006604bc366
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-08-01 09:58:52 +0000

    Tue, Aug 1, 2023, 11:58 AM +02:00

---
+++ gryffindors.py
@@ -0,0 +1,14 @@
+students = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+    {"name": "Ron", "house": "Gryffindor"},
+]
+
+
+gryffindors = [
+    student["name"] for student in students if student["house"] == "Gryffindor"
+]
+
+print(gryffindors)
--- map.py
+++ map.py
@@ -19,12 +19,22 @@ def yell(*words):
 
 main()
 """
-
+"""
 def main():
     yell("This", "is", "CS50!")
 
 def yell(*words):
     uppercased = map(str.upper, words) # without () not ccalling it now.. map maps upper to all the words... returns brand new list with applied function
+    print(*uppercased)
+
+main()
+""""
+######| List comprehensions |######
+def main():
+    yell("This", "is", "CS50!")
+
+def yell(*words):
+    uppercased = [word.upper() for word in words]
     print(*uppercased)
 
 main()
