08eba47a3bd66f9a67736c9e14cb63f63c71c685
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-08-01 10:20:33 +0000

    Tue, Aug 1, 2023, 12:20 PM +02:00

--- inheritance.py
+++ inheritance.py
@@ -1,22 +1,23 @@
 ##redundunace, ne takhle##
 
-#class Student:
- #   def __init__(self, name, house):
+
+# class Student:
+#   def __init__(self, name, house):
 #        self.name = name
 #        self.house = house
 #
 #
-#class Professor:
+# class Professor:
 #    def __init__(self, name, subject):
 #        self.name = name
- #       self.subject = subject
+#       self.subject = subject
 ########################################
 ########### | Inheritance |#############
 class Wizard:
     def __init__(self, name):
         if not name:
             raise ValueError
-        self.name = name ##mutual variable
+        self.name = name  ##mutual variable
 
 
 class Professor(Wizard):
@@ -26,12 +27,14 @@ class Professor(Wizard):
         super().__init__(name)
         self.subject = subject
 
+
 class Student(Wizard):
     def __init__(self, name, house):
         if not house:
             raise ValueError
         super().__init__(name)
         self.house = house
+
 
 student = Student("Harry", "Gryffindor")
 professor = Professor("Severus", "Defence against dark arts")
