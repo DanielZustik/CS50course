fa0626aeb49ac78b56a956e8fe2c1a0f03880cc7
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-08-01 10:36:44 +0000

    Tue, Aug 1, 2023, 12:36 PM +02:00

--- dictcomper.py
+++ dictcomper.py
@@ -8,9 +8,11 @@ for student in students:
     gryffindors.append({"name": student, "house": "Gryffindor"})
 
 """
-
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students] # quicker way with dict comprehension
+"""
+gryffindors = [{"name": student, "house": "Gryffindor"} for student in students] # quicker way with list comprehension
                                                                                 # no cconditional this time, and on the left is what i want
                                                                                 #and on the right is what from / for what all
+"""
+gryffindors = {student: "Gryffindor" for student in students} #dict comprehension
 
 print(gryffindors)
