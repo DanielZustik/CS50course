ef1f0b6f5ec3f3afc40421629d47554f8f31b3c2
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-08-01 10:34:12 +0000

    Tue, Aug 1, 2023, 12:34 PM +02:00

--- dictcomper.py
+++ dictcomper.py
@@ -10,5 +10,7 @@ for student in students:
 """
 
 gryffindors = [{"name": student, "house": "Gryffindor"} for student in students] # quicker way with dict comprehension
+                                                                                # no cconditional this time, and on the left is what i want
+                                                                                #and on the right is what from / for what all
 
 print(gryffindors)
