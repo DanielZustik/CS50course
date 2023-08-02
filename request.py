f4b44fa58d737fb0a3725ea64ed664fce63cede4
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-08-02 10:12:53 +0000

    Wed, Aug 2, 2023, 12:12 PM +02:00

--- request.py
+++ request.py
@@ -4,4 +4,5 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + )
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response)
