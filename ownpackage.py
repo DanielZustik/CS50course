18e24c364ce7a8eb629cb3b26fb8383ef7baffb7
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-08-02 10:51:22 +0000

    Wed, Aug 2, 2023, 12:51 PM +02:00

--- ownpackage.py
+++ ownpackage.py
@@ -11,5 +11,6 @@ def goodbye(name):
     print(f"goodbye, {name}")
 
 
-if __name__ == "__name__": 
-    main()
+if __name__ == "__name__": #so __name__ variable is automaticlly beign set to "__name__" if running this file/program from termina;
+    main()                  # otherwise the other file/program which is actually beign executed has it... meaning if i just export functions
+                            # from here, the mani() wont be called
