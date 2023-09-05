161497346cdb89b892687668ebcc4ec84c76dcd9
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2023-09-05 12:31:44 +0000

    Tue, Sep 5, 2023, 2:31 PM +02:00

--- CS50W/week1/app.py
+++ CS50W/week1/app.py
@@ -1,5 +1,16 @@
-from flask import Flask
 
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
 app = Flask(__name__)
 app.run
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    ...
