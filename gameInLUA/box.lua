001668e0a879e4e8d520e7f483a8f97ecd3a2503
DanielZustik <125804167+DanielZustik@users.noreply.github.com>
2024-02-11 14:05:00 +0100

    Delete gameInLUA directory

--- gameInLUA/box.lua
+++
@@ -1,5 +0,0 @@
-Box = Entity:extend()
-
-function Box:new(x, y, name)
-  Box.super.new(self, x, y, "images/3 Objects/Other/Box1.png", name)
-end
--- gameInLUA/money.lua
+++
@@ -1,7 +0,0 @@
-Money = Entity:extend()
-
-function Money:new(x, y)
-  Money.super.new(self, x, y, "images/3 Objects/Other/Box3.png", name)
-  self.name = "money"
-
-end
